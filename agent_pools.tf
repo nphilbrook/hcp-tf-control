b00121ca5d23772c751c6e0341726c1e0aefac2f
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-03-26 15:02:07 -0400

    agent mode for these workspaces

--- agent_pools.tf
+++ agent_pools.tf
@@ -1,10 +1,5 @@
-import {
-  to = tfe_agent_pool.aws
-  id = "apool-fXJebpvzkKGqaCqN"
-}
-
 resource "tfe_agent_pool" "aws" {
   name                = "bu-lab-usw2"
   organization        = "philbrook"
-  organization_scoped = false
+  organization_scoped = true
 }
