7fce31346727e754d227e483f0d014f60c7a5112
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-03-26 06:29:15 -0400

    add variables for HCP workload identity

--- hcp_vault_ws.tf
+++ hcp_vault_ws.tf
@@ -30,18 +30,52 @@ resource "tfe_workspace" "hcp_vault_terraform" {
 
 resource "tfe_variable" "hcp_ws_namespace" {
   workspace_id = tfe_workspace.hcp_vault_terraform.id
-  description  = "Vault Namespace for this workspace"
-  key          = "TFC_VAULT_NAMESPACE"
-  value        = "admin"
-  category     = "env"
+
+  key      = "TFC_VAULT_NAMESPACE"
+  value    = "admin"
+  category = "env"
+
+  description = "Vault Namespace for this workspace"
 }
 
 resource "tfe_variable" "hcp_ws_vault_admin_run_role" {
   workspace_id = tfe_workspace.hcp_vault_terraform.id
-  key          = "TFC_VAULT_RUN_ROLE"
-  value        = "hcp-tf-admin"
-  category     = "env"
-  description  = "Vault JWT role for this workspace"
+
+  key      = "TFC_VAULT_RUN_ROLE"
+  value    = "hcp-tf-admin"
+  category = "env"
+
+  description = "Vault JWT role for this workspace"
+}
+
+resource "tfe_variable" "hcp_workload_identity" {
+  workspace_id = tfe_workspace.hcp_vault_terraform.id
+
+  key      = "TFC_HCP_PROVIDER_AUTH"
+  value    = "true"
+  category = "env"
+
+  description = "Enable HCP workload identity for this workspace"
+}
+
+resource "tfe_variable" "hcp_workload_identity_audience" {
+  workspace_id = tfe_workspace.hcp_vault_terraform.id
+
+  key      = "TFC_HCP_WORKLOAD_IDENTITY_AUDIENCE"
+  value    = "hcp.workload.identity"
+  category = "env"
+
+  description = "HCP workload identity audience"
+}
+
+resource "tfe_variable" "hcp_workload_identity_resource" {
+  workspace_id = tfe_workspace.hcp_vault_terraform.id
+
+  key      = "TFC_HCP_RUN_PROVIDER_RESOURCE_NAME"
+  value    = "iam/project/bbb3c5e5-262e-4171-8747-bf5da61f75d1/service-principal/hcp-vault-tf/workload-identity-provider/hcp-tf-dynamic-credentials"
+  category = "env"
+
+  description = "HCP workload identity resource name"
 }
 
 
