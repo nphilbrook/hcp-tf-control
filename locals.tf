8c8bc36fba222d753a016e460b627db8c42e4300
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-03 12:56:09 -0400

    remove trailing slash

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  vault_address = "https://vault-cluster-private-vault-e9bf486c.f4702644.z1.hashicorp.cloud:8200/"
+  vault_address = "https://vault-cluster-private-vault-e9bf486c.f4702644.z1.hashicorp.cloud:8200"
   project_id    = "prj-jwafYpMw2Nb6m2Zd"
   top_level_namespaces = toset([
     "testing",
