ce999e8c4d171977ea2e99d5c21cceae70acb03d
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-01 13:09:33 -0400

    Add the freaking data file

---
+++ data.tf
@@ -0,0 +1,21 @@
+# Automatically provided from HCP Terraform, BUT ONLY if this is a root module
+variable "TFC_WORKSPACE_SLUG" {
+  type = string
+}
+
+locals {
+  slug_parts = split("/", var.TFC_WORKSPACE_SLUG)
+  org_name   = local.slug_parts[0]
+  ws_name    = local.slug_parts[1]
+}
+
+# Get a reference to our *own* workspace ID
+# ref locals.tf for
+data "tfe_workspace" "this" {
+  organization = local.org_name
+  name         = local.ws_name
+}
+
+output "ws_id" {
+  value = data.tfe_workspace.this.id
+}
